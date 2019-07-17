4bd61dfdb789d54c5ed8e11e6300f5e7f5834073
Bilal <bilaltariq20@hotmail.com>
2019-07-17 23:24:58 +0500

    Mix and Match Xml Added

---
+++ app/src/main/java/com/example/notifyme/activity/MixandMatchActivity.java
@@ -0,0 +1,15 @@
+package com.example.notifyme.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.notifyme.R;
+
+public class MixandMatchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mixand_match);
+    }
+}
